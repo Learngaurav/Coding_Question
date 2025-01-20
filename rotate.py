7d3b0a8aa510821df6a86b831b3230bb8e418e2b
Masgaurav <gauravkr878@gmail.com>
2025-01-20 22:01:12 +0530

    rotate array

---
+++ rotate.py
@@ -0,0 +1,10 @@
+from sys import *
+from collections import *
+from math import *
+
+def rotateArray(arr: [], n: int) -> []:
+    # Write your code from here.
+    first_element= arr.pop(0)
+    last_element= arr.append(first_element) 
+    
+    return arr  
